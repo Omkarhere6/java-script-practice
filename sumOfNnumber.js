8abd6220c3529e5f8fd8ba20d8fcc39cd7219966
Omkar Hawaldar <o.hawaldar@bm-systems.in>
2025-01-14 09:54:52 +0530

    formula method for sum of n numbers

--- sumOfNnumber.js
+++ sumOfNnumber.js
@@ -3,7 +3,15 @@ function sumOfnNumbers(n){
     for(let i = 0 ; i <= n ;i++){
         totalSum += i ;
     }
+    console.log("Using the generic method :");
     console.log(totalSum);
+
+    //can also use this logic to print the sum of n numbers
+    console.log("Using the formula method :");
+    console.log(n*(n+1)/2)
+
 } 
 
 sumOfnNumbers(9);
+sumOfnNumbers(11);
+sumOfnNumbers(10);
